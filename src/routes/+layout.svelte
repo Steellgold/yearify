626fdec32a8bf3e5f0a5eb48e72d73f1907b07ef
Gaëtan H <steellgold0@gmail.com>
2023-08-30 12:39:33 +0200

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import "../app.css";
+
+  export let title: string = "Year to Words";
+  export let siteName: string = "Get year to words";
+  export let description: string = "Type the year in the input field and get the correct pronunciation in English.";
+  export let url: string = "https://wordsyears.vercel.app";
+  export let keywords: string = "sveltekit, svelte, sveltekit starter, sveltekit template, sveltekit typescript, sveltekit tailwindcss";
+  export let color: string = "#0F172A";
+</script>
+
+<title>{title}</title>
+<meta name="description" content={description} />
+<meta name="keywords" content={keywords} />
+<meta name="og:title" content={title} />
+<meta name="og:site_name" content={siteName} />
+<meta name="og:description" content={description} />
+<meta name="og:url" content={url} />
+<meta name="og:type" content="website" />
+<meta name="twitter:site" content="@Steellgold" />
+<meta name="twitter:creator" content="@Steellgold" />
+<meta name="twitter:title" content={title} />
+<meta name="twitter:description" content={description} />
+<meta name="theme-color" content={color} />
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  import { toWords } from "number-to-words";
+  import { onMount } from "svelte";
+  
+  export let year: number = new Date().getFullYear();
+  export let yearWords: string = "";
+  
+  let loading = true;
+
+  onMount(() => {
+    if (location.search) {
+      const params = new URLSearchParams(location.search);
+      const yearParam = params.get("year");
+      if (yearParam) {
+        year = parseInt(yearParam);
+        yearWords = numberToWords(year);
+      }
+    }
+
+    loading = false;
+  });
+
+  const numberToWords = (number: number) => {
+    return toWords(number);
+  };
+</script>
+
+<div class="flex items-center justify-center mt-10">
+  <div class="block max-w-sm p-6 border rounded-lg shadow bg-gray-800 border-gray-700">
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-white">How to say this year?</h5>
+    <p class="font-normal text-gray-400">
+      Type the year in the input field and get the correct pronunciation in English.
+    </p>
+
+    <hr class="my-4 border-gray-300 dark:border-gray-700">
+
+    {#if loading}
+      <div class="h-10 bg-gray-200 rounded-md dark:bg-gray-700 mb-4"></div>
+      <div class="h-10 bg-gray-200 rounded-md dark:bg-gray-700"></div>
+    {:else}
+      <input
+        type="number"
+        class="w-full px-4 py-2 mb-4 border rounded-lg bg-gray-800 border-gray-700 text-gray-200 focus:outline-none focus:shadow-outline"
+        placeholder="Type the year here"
+        bind:value={year}
+        on:input={() => yearWords = numberToWords(year)}
+      />
+
+      <input
+        type="text"
+        disabled
+        class="w-full px-4 py-2 border rounded-lg bg-gray-800 border-gray-700 text-gray-200 focus:outline-none focus:shadow-outline"
+        value={yearWords}
+      />
+    {/if}
+  </div>
+</div>
